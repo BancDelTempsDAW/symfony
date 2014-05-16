743ab9836669c625033a434731e6f48712a9a993
oscar <omonroma@gmail.com>
2014-05-16 13:20:23 +0200

    Formulari casi acabat

--- src/bonavall/BancdeltempsBundle/Controller/AltaUsuariController.php
+++ src/bonavall/BancdeltempsBundle/Controller/AltaUsuariController.php
@@ -2,19 +2,38 @@
 
 namespace bonavall\BancdeltempsBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+/*use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use bonavall\BancdeltempsBundle\Entity\Bancdeltemps;
-use bonavall\BancdeltempsBundle\Form\BancdeltempsType;
+use bonavall\BancdeltempsBundle\Form\BancdeltempsType;*/
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use bonavall\BancdeltempsBundle\Form\AltaUsuariType;
 
 
-class AltausuariController extends Controller
+class AltaUsuariController extends Controller
 {
     public function registreAction()
     {
-        return $this->render('bonavallBancdeltempsBundle:Usuari:altaUsuari.html.twig', array());
+        $form = $this->get('form.factory')->create(
+            new AltaUsuariType(),
+            array()
+        );
+
+        $request = $this->get('request');
+
+        if ($request->getMethod() == 'POST')
+        {
+            $form->bindRequest($request);
+            if ($form->isValid())
+            {
+                //Implementacion de qué queremos hacer con el formulario, ingresar en BB.DD,...
+            }
+        }
+
+        return $this->render('bonavallBancdeltempsBundle:Usuari:altausuari.html.twig',
+            array('form' => $form->createView())
+        );
     }
 }
--- src/bonavall/BancdeltempsBundle/Form/AltaUsuariType.php
+++ src/bonavall/BancdeltempsBundle/Form/AltaUsuariType.php
@@ -4,17 +4,55 @@ namespace bonavall\BancdeltempsBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+/*use Symfony\Component\OptionsResolver\OptionsResolverInterface;*/
 
-class BancdeltempsType extends AbstractType
+//class AltaUsuariType extends AbstractType
+class AltaUsuariType extends AbstractType
 {
-    public function buildForm()
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $builder->add('nom', 'text', array())
+                ->add('cognoms')
+                ->add('direccio')
+                ->add('codi_postal')
+                ->add('poblacio')
+                ->add('dni', 'text', array('label' => 'DNI'))
+
+
+
+                // Arreglar correo para que funciones el formulario
+                ->add('correu', array(
+                    'type' => 'email',
+                    'options' => array('label' => 'Correu Electrònic: '),
+                    'required' => true
+                ))
+
+                ->add('password', 'repeated', array(
+                    'type' => 'password',
+                    'invalid_message' => 'Les dos contrasenyes han de ser la mateixa.',
+                    'options' => array(
+                        'attr' => array('class' => 'password-field')),
+                    'required' => true,
+                    'first_options'  => array('label' => 'Contrasenya: '),
+                    'second_options' => array('label' => 'Repeteix Contrasenya: '),
+                ));
 
     }
 
-    public function getDefaultOptions()
+    public function getDefaultOptions(array $options) //array $options
     {
+        return array(
+            'data_class' => 'bonavall\BancdeltempsBundle\Entity\Usuari'
+        );
+    }
 
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        // TODO: Implement getName() method.
     }
 }
