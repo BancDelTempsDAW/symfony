4955f3199f1d0aef8d4908f87884425a0c2eb862
ricardbo <ricardbarberan@gmail.com>
2014-05-19 13:39:41 +0200

    Ricard: "Casi terminat update BD usuari"

--- src/bonavall/BancdeltempsBundle/Controller/UsuariPerfilController.php
+++ src/bonavall/BancdeltempsBundle/Controller/UsuariPerfilController.php
@@ -25,6 +25,20 @@ class UsuariPerfilController extends Controller {
 
             $usuari = $em->getRepository('bonavallBancdeltempsBundle:Usuari')->findOneById(5);
             if (is_object($usuari)) {
+                
+//                $user = new Usuari();
+//                $user->setPassword(1234);
+//                $user->setNom("Ricard");
+//                $user->setCognom("asdf");
+//                $user->setAdreca("C/adsf");
+//                $user->setTelefon(682155466);
+//                $user->setEmail("asdf@asdf.com");
+//                $user->setPresentacio("asdfadsf");
+//
+//                $em = $this->getDoctrine()->getManager();
+//                $em->persist($user);
+//                $em->flush();
+                
                 $request = $this->get('request');
                 $return = array(
                     'responseCode' => 200,
