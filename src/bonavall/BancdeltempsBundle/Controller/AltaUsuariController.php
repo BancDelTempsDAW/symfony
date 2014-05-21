81e377ea07286b4a7924be7155614d02c4260013
oscar <omonroma@gmail.com>
2014-05-21 21:00:47 +0200

    Oscar: Ja funciona el registre

--- src/bonavall/BancdeltempsBundle/Controller/AltaUsuariController.php
+++ src/bonavall/BancdeltempsBundle/Controller/AltaUsuariController.php
@@ -4,13 +4,21 @@ namespace bonavall\BancdeltempsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use bonavall\BancdeltempsBundle\Form\AltaUsuariType;
+use bonavall\BancdeltempsBundle\Entity\Usuari;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 
 class AltaUsuariController extends Controller
 {
-    public function registreAction()
+    /*public function sferegistrevellAction()
     {
-        $form = $this->get('form.factory')->create(new AltaUsuariType(), array());
+        $form = $this->get('form.factory')->create(
+            new AltaUsuariType(),
+            array()
+        );
         $request = $this->get('request');
 
         if ($request->getMethod() == 'POST')
@@ -29,35 +37,101 @@ class AltaUsuariController extends Controller
                     echo "Vacio";
                 }
 
-            // Introducir datos en BB.DD.
-
-
-        /*        // Mensaje para notificar al usuario que todo ha salido bien
-                $session = $this->get('request')->getSession();
-                $session->setFlash('notice', 'Gracias por registrarte en Desymfony 2011');
-
-                // Obtenemos el usuario
-                $usuario = $form->getData();
-
-                // Codificamos el password
-                $factory = $this->get('security.encoder_factory');
-                $codificador = $factory->getEncoder($usuario);
-                $password = $codificador->encodePassword($usuario->getPassword(), $usuario->getSalt());
-                $usuario->setPassword($password);
-
-                // Guardamos el objeto en base de datos
-                $em = $this->get('doctrine')->getEntityManager();
-                $em->persist($usuario);
-                $em->flush();
-
-                // Logueamos al usuario
-                $token = new UsernamePasswordToken($usuario, null, 'main', $usuario->getRoles());
-                $this->get('security.context')->setToken($token);
-
-                return $this->redirect($this->generateUrl('portada')); */
             }
         }
         return $this->render('bonavallBancdeltempsBundle:Usuari:altausuari.html.twig',
             array('form' => $form->createView()));
+    }*/
+
+    /**
+     * Finds and displays a Usuari entity.
+     *
+     * @Route("/{id}", name="usuari_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('bonavallBancdeltempsBundle:Usuari')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Usuari entity.');
+        }
+
+        //$deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            //'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Creates a new Usuari entity.
+     *
+     * @Route("/", name="alta_usuari_create")
+     * @Method("POST")
+     * @Template("bonavallBancdeltempsBundle:AltaUsuari:new.html.twig")
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Usuari();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('alta_usuari_show', array('id' => $entity->getId())));
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+
+    /**
+     * Creates a form to create a Usuari entity.
+     *
+     * @param Usuari $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Usuari $entity)
+    {
+        $form = $this->createForm(new AltaUsuariType(), $entity, array(
+            'action' => $this->generateUrl('alta_usuari_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Registrat'));
+
+        return $form;
+    }
+
+
+    /**
+     * Displays a form to create a new Usuari entity.
+     *
+     * @Route("/altausuari/", name="alta_usuari")
+     * @Method("GET")
+     * @Template()
+     */
+    public function registreAction(){
+        $entity = new Usuari();
+        $form   = $this->createCreateForm($entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+
+
     }
 }
--- src/bonavall/BancdeltempsBundle/Form/AltaUsuariType.php
+++ src/bonavall/BancdeltempsBundle/Form/AltaUsuariType.php
@@ -4,13 +4,14 @@ namespace bonavall\BancdeltempsBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class AltaUsuariType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom', 'text', array('label' => 'Nom: '))
+            /*->add('nom', 'text', array('label' => 'Nom: '))
             ->add('cognom', 'text', array('label' => 'Cognoms: '))
             ->add('adreca', 'textarea', array('label' => 'Adreça: '))
             ->add('poblacio', 'text', array('label' => 'Població: '))
@@ -24,7 +25,23 @@ class AltaUsuariType extends AbstractType
                     'attr' => array('class' => 'password-field')),
                 'first_options'  => array('label' => 'Contrasenya: '),
                 'second_options' => array('label' => 'Repeteix Contrasenya: ')))
-            ->add('fotografia', 'file', array('label' => 'Fotografia: '));
+            ->add('fotografia', 'file', array('label' => 'Fotografia: '));*/
+            //->add('salt')
+            ->add('email')
+            ->add('password')
+            ->add('isActive')
+            ->add('nom')
+            ->add('cognom')
+            ->add('adreca')
+            ->add('poblacio')
+            ->add('codi_postal')
+            ->add('telefon')
+            ->add('fotografia')
+            ->add('password')
+            ->add('presentacio')
+            ->add('punts')
+            ->add('rol')
+            ;
     }
 
     public function getDefaultOptions(array $options)
@@ -43,7 +60,7 @@ class AltaUsuariType extends AbstractType
     public function getName()
     {
         //return 'bonavall_bancdeltemps_altausuari';
-        return 'nom';
+        return 'bonavall_bancdeltempsbundle_altausuari';
         // TODO: Implement getName() method.
     }
 }
