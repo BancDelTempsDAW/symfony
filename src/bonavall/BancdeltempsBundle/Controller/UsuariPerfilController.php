30f3c8c1efb12a62d7551bcaee0be19f810e18cc
ricardbo <ricardbarberan@gmail.com>
2014-05-23 19:08:57 +0200

    Ricard: Modificar Perfil fet amb form

--- src/bonavall/BancdeltempsBundle/Controller/UsuariPerfilController.php
+++ src/bonavall/BancdeltempsBundle/Controller/UsuariPerfilController.php
@@ -10,51 +10,114 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 //use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 //use bonavall\BancdeltempsBundle\Entity\Usuari;
 //use bonavall\BancdeltempsBundle\Form\UsuariType;
-use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\HttpFoundation\Response;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use bonavall\BancdeltempsBundle\Entity\Usuari;
+use bonavall\BancdeltempsBundle\Form\UserType;
 
 class UsuariPerfilController extends Controller {
-
+    
     /**
-     * Return a ajax response
+     * Displays a form to edit an existing Usuari entity.
+     *
+     * @Route("/{id}/edit", name="usuari_edit")
+     * @Method("GET")
+     * @Template()
      */
-    public function perfilAction(Request $request) {
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
 
-        if ($request->isXmlHttpRequest()) {
+        $entity = $em->getRepository('bonavallBancdeltempsBundle:Usuari')->find($id);
 
-            $em = $this->getDoctrine()->getManager();
-            $userId = $this->get('security.context')->getToken()->getUser()->getId();
-            $usuari = $em->getRepository('bonavallBancdeltempsBundle:Usuari')->findOneById($userId);
-
-            if (is_object($usuari)) {
-                                
-                $usuari->setFotografia($request->request->get('fotografia'));
-                $usuari->setPassword($request->request->get('password'));
-                $usuari->setNom($request->request->get('nom'));
-                $usuari->setCognom($request->request->get('cognom'));
-                $usuari->setAdreca($request->request->get('adreca'));
-                $usuari->setTelefon($request->request->get('telefon'));
-                $usuari->setEmail($request->request->get('email'));
-                $usuari->setPresentacio($request->request->get('presentacio'));
-
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($usuari);
-                $em->flush();
-
-                //$request = $this->get('request');
-                $return = array(
-                    'responseCode' => 200,
-                );
-            } else {
-                $return = array(
-                    'responseCode' => 400,
-                );
-            }
-            return new Response(json_encode($return), $return['responseCode']);
-        } else {
-            return $this->render('bonavallBancdeltempsBundle:user:perfil.html.twig', array('aaa' => $request));
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Usuari entity.');
         }
-    }
 
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+    
+    /**
+    * Creates a form to edit a Usuari entity.
+    *
+    * @param Usuari $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Usuari $entity)
+    {
+        $form = $this->createForm(new UserType(), $entity, array(
+            'action' => $this->generateUrl('user_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    
+    /**
+     * Creates a form to delete a Usuari entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('usuari_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
+            
+    /**
+     * Edits an existing Usuari entity.
+     *
+     * @Route("/{id}", name="usuari_update")
+     * @Method("PUT")
+     * @Template("bonavallBancdeltempsBundle:Usuari:edit.html.twig")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('bonavallBancdeltempsBundle:Usuari')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Usuari entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('perfil_user', array('id' => $id)));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+    
     public function baixaAction() {
 
         $em = $this->getDoctrine()->getManager();
@@ -81,5 +144,46 @@ class UsuariPerfilController extends Controller {
         }
         //return $this->render('bonavallBancdeltempsBundle:user:baixaPerfil.html.twig', array());
     }
+    
+    /**
+     * Return a ajax response
+     */
+//    public function perfilAction(Request $request) {
+//
+//        if ($request->isXmlHttpRequest()) {
+//
+//            $em = $this->getDoctrine()->getManager();
+//            $userId = $this->get('security.context')->getToken()->getUser()->getId();
+//            $usuari = $em->getRepository('bonavallBancdeltempsBundle:Usuari')->findOneById($userId);
+//
+//            if (is_object($usuari)) {
+//                                
+//                $usuari->setFotografia($request->request->get('fotografia'));
+//                $usuari->setPassword($request->request->get('password'));
+//                $usuari->setNom($request->request->get('nom'));
+//                $usuari->setCognom($request->request->get('cognom'));
+//                $usuari->setAdreca($request->request->get('adreca'));
+//                $usuari->setTelefon($request->request->get('telefon'));
+//                $usuari->setEmail($request->request->get('email'));
+//                $usuari->setPresentacio($request->request->get('presentacio'));
+//
+//                $em = $this->getDoctrine()->getManager();
+//                $em->persist($usuari);
+//                $em->flush();
+//
+//                //$request = $this->get('request');
+//                $return = array(
+//                    'responseCode' => 200,
+//                );
+//            } else {
+//                $return = array(
+//                    'responseCode' => 400,
+//                );
+//            }
+//            return new Response(json_encode($return), $return['responseCode']);
+//        } else {
+//            return $this->render('bonavallBancdeltempsBundle:user:perfil.html.twig', array());
+//        }
+//    }
 
 }
