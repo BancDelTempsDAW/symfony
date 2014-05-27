68d34369700444a46243c763a677efcc40e445de
ricardbo <ricardbarberan@gmail.com>
2014-05-27 19:43:21 +0200

    Ricard: perfilat canvis de presentacio en modificar perfil

--- web/js/validacioPerfil.js
+++ web/js/validacioPerfil.js
@@ -1,30 +1,33 @@
 $(document).ready(function() {
     
-    $("#bonavall_bancdeltempsbundle_usuari_password").hide();
-    $("#bonavall_bancdeltempsbundle_usuari_submit").hide();
-    $("#labelConfirm").hide();
+    $("#confirmError").hide();
     $("#equalError").hide();
     
-    $("#user_pass").keypress(function(){
-        $("#labelConfirm").show();
-        $("#bonavall_bancdeltempsbundle_usuari_password").show();
+    $("#bonavall_bancdeltempsbundle_usuari_submit").click(function(){
+        
+       var pass1 = $("#user_pass").val();
+       var pass2 = $("#bonavall_bancdeltempsbundle_usuari_password").val();
+        
+       if(pass1 != pass2){
+            $("#confirmError").show();
+        }else{
+            $("#confirmError").hide();
+            $("#equalError").hide();
+        }
     });
     
-    $("#bonavall_bancdeltempsbundle_usuari_password").blur(function(){
+    $("#bonavall_bancdeltempsbundle_usuari_password").keyup(function(){
         
-        var pass1 = $("#user_pass").val();;
-        var pass2 = $("#bonavall_bancdeltempsbundle_usuari_password").val();;
-
+        var pass1 = $("#user_pass").val();
+        var pass2 = $("#bonavall_bancdeltempsbundle_usuari_password").val();
+        
         if(pass1 != pass2){
-            $("#bonavall_bancdeltempsbundle_usuari_submit").hide();
             $("#equalError").show();
             return false;
         }else{
             $("#equalError").hide();
-            $("#bonavall_bancdeltempsbundle_usuari_submit").show();
+            $("#confirmError").hide();
             return true;
         }
     });
-        
-
 });
